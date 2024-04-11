4264deabd63ff9f93118962c84dafb847f97a506
alecmorrison <alecmorrison@umail.ucsb.edu>
2024-04-11 00:12:11 -0700

    am - structure for main

--- assign-1/main.cpp
+++ assign-1/main.cpp
@@ -2,6 +2,21 @@
 using namespace std;
 
 int main() {
-    cout << "HELLO" << endl;
+    // Initialize Regex for every token 
+
+    // Convert to NFA data structure with converter function
+    
+
+    /*
+    loop through input string finding longest token, pop token from beginning of string and 
+    repeat for the rest
+
+    OR
+
+    Keep track of NFA state when it is impossible to reach accept state 
+    (e.g. reads character that has no defined transition)
+    */ 
+
+
     return 0;
 }
