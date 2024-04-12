fda4965427057b457e5d2d1f237b129c0a8458e2
Ron Kibel <ron.kibel1@gmail.com>
2024-04-12 13:29:24 -0700

    some optimizations to ugly code

--- assign-1/main.cpp
+++ assign-1/main.cpp
@@ -3,6 +3,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 int main(int argc, char** argv) {
     if (argc < 2) {
@@ -22,44 +23,36 @@ int main(int argc, char** argv) {
     }
 
     std::string::size_type i = 0;
-    int length = 1;
     std::string token = "Error";
-    std::vector<std::string> res;
-    while (i < input.size()) {
-        if (isdigit(input[i])) {
+    int length = 1;
+    const std::string::size_type n = input.size();
+    while (i < n) {
+        const char c = input[i];
+        if (isdigit(c)) {
             std::string::size_type iend = i+1;
-            while (iend < input.size() && isdigit(input[iend])) iend++;
+            while (iend < n && isdigit(input[iend])) iend++;
             length = iend - i;
             token = "Num";
         }
-        else if (isalpha(input[i])) {
+        else if (isalpha(c)) {
             std::string::size_type iend = i+1;
-            while (iend < input.size() && (isalpha(input[iend]) || isdigit(input[iend]))) {
-                iend++;
-            }
+            while (iend < n && (isalpha(input[iend]) || isdigit(input[iend]))) iend++;
             length = iend - i;
-            if (input.substr(i, length) == "int") token = "Int";
-            else if (input.substr(i, length) == "struct") token = "Struct";
-            else if (input.substr(i, length) == "nil") token = "Nil";
-            else if (input.substr(i, length) == "break") token = "Break";
-            else if (input.substr(i, length) == "continue") token = "Continue";
-            else if (input.substr(i, length) == "return") token = "Return";
-            else if (input.substr(i, length) == "if") token = "If";
-            else if (input.substr(i, length) == "else") token = "Else";
-            else if (input.substr(i, length) == "while") token = "While";
-            else if (input.substr(i, length) == "new") token = "New";
-            else if (input.substr(i, length) == "let") token = "Let";
-            else if (input.substr(i, length) == "extern") token = "Extern";
-            else if (input.substr(i, length) == "fn") token = "Fn";
-            else token = "Id";
+            static const std::unordered_map<std::string, std::string> tokenMap = {
+                {"int", "Int"}, {"struct", "Struct"}, {"nil", "Nil"}, {"break", "Break"},
+                {"continue", "Continue"}, {"return", "Return"}, {"if", "If"}, {"else", "Else"},
+                {"while", "While"}, {"new", "New"}, {"let", "Let"}, {"extern", "Extern"}, {"fn", "Fn"}
+            };
+            auto it = tokenMap.find(input.substr(i, length));
+            token = (it == tokenMap.end()) ? "Id" : it->second;
         }
-        else if (input[i] == ' ' || input[i] == '\t' || input[i] == '\n' || input[i] == '\r') {
+        else if (c == ' ' || c == '\t' || c == '\n' || c == '\r') {
             std::string::size_type iend = i+1;
-            while (iend < input.size() && (input[iend] == ' ' || input[iend] == '\t' || input[iend] == '\n' || input[iend] == '\r')) iend++;
+            while (iend < n && (input[iend] == ' ' || input[iend] == '\t' || input[iend] == '\n' || input[iend] == '\r')) iend++;
             length = iend - i;
             token = "whitespace";
         }
-        else if (input[i] == '/') {
+        else if (c == '/') {
             if (i+1 < input.length() && input[i+1] == '/') {
                 std::string::size_type iend = i+2;
                 while (iend < input.length() && input[iend] != '\n') iend++;
@@ -68,8 +61,8 @@ int main(int argc, char** argv) {
             }
             else if (i+1 < input.length() && input[i+1] == '*') {
                 std::string::size_type iend = i+2;
-                while (iend < input.size() && !(iend < input.size() - 1 && input[iend] == '*' && input[iend+1] == '/')) iend++;
-                if (iend < input.size()-1 && input[iend] == '*' && input[iend+1] == '/') {
+                while (iend < n && !(iend < n-1 && input[iend] == '*' && input[iend+1] == '/')) iend++;
+                if (iend < n-1 && input[iend] == '*' && input[iend+1] == '/') {
                     length = iend - i + 2;
                     token = "c-comment";
                 }
@@ -80,7 +73,7 @@ int main(int argc, char** argv) {
             }
             else { token = "Slash"; }
         }
-        switch(input[i]) {
+        switch(c) {
             case '&': token = "Address"; break;
             case ':': token = "Colon"; break;
             case ';': token = "Semicolon"; break;
@@ -88,38 +81,23 @@ int main(int argc, char** argv) {
             case '_': token = "Underscore"; break;
             case '+': token = "Plus"; break;
             case '-': 
-                if (i+1 < input.size() && input[i+1] == '>') {
-                    length = 2;
-                    token = "Arrow";
-                }
+                if (i+1 < n && input[i+1] == '>') { length = 2; token = "Arrow"; }
                 else { token = "Dash"; }
                 break;
             case '*': token = "Star"; break;
             case '=': 
-                if (i+1 < input.size() && input[i+1] == '=') {
-                    length = 2;
-                    token = "Equal";
-                }
+                if (i+1 < n && input[i+1] == '=') { length = 2; token = "Equal"; }
                 else { token = "Gets"; }
                 break;            
             case '!': 
-                if (i+1 < input.size() && input[i+1] == '=') {
-                    length = 2;
-                    token = "NotEq";
-                }
+                if (i+1 < n && input[i+1] == '=') { length = 2; token = "NotEq"; }
                 break;
             case '<':
-                if (i+1 < input.size() && input[i+1] == '=') {
-                    length = 2;
-                    token = "Lte";
-                }
+                if (i+1 < n && input[i+1] == '=') { length = 2; token = "Lte"; }
                 else { token = "Lt"; }
                 break;
             case '>':
-                if (i+1 < input.size() && input[i+1] == '=') {
-                    length = 2;
-                    token = "Gte";
-                }
+                if (i+1 < n && input[i+1] == '=') { length = 2; token = "Gte"; }
                 else { token = "Gt"; }
                 break;
             case '.': token = "Dot"; break;
@@ -131,19 +109,11 @@ int main(int argc, char** argv) {
             case '}': token = "CloseBrace"; break;
         }
         
-
-        if (token == "Id" || token == "Num") {
-            res.push_back(token + "(" + input.substr(i, length) + ")");
-        }
-        else if (token != "whitespace" && token != "c++-comment" && token != "c-comment" && token != "unclosed-c-comment") {
-            res.push_back(token);
-        }
+        if (token == "Id" || token == "Num")  std::cout << token << "(" << input.substr(i, length) << ")" << std::endl;
+        else if (token != "whitespace" && token != "c++-comment" && token != "c-comment" && token != "unclosed-c-comment") std::cout << token << std::endl;
         i += length;
         length = 1;
         token = "Error";
-    }
-    for (std::string str: res) {
-        std::cout << str << std::endl;
     }
     return 0;
 }
